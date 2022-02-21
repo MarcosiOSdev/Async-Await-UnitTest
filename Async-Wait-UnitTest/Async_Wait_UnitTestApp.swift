75e77470736f9bf59452b6c50b44e78579bf04b1
Marcos Felipe <mfelipesp@gmail.com>
2022-02-20 21:36:24 -0300

    Initial Commit

---
+++ Async-Wait-UnitTest/Async_Wait_UnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Wait_UnitTestApp.swift
+//  Async-Wait-UnitTest
+//
+//  Created by marcos.felipe.souza on 20/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Async_Wait_UnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
